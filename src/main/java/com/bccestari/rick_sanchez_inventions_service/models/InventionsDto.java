794e5cfe3979a67f721af815fdaa86b88e9694c3
Bruno Cestari <bccestari.project@gmail.com>
2024-09-07 13:09:25 -0300

    Create data transfer object for inventions

---
+++ src/main/java/com/bccestari/rick_sanchez_inventions_service/models/InventionsDto.java
@@ -0,0 +1,55 @@
+package com.bccestari.rick_sanchez_inventions_service.models;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class InventionsDto {
+    @NotEmpty (message = "The name is required.")
+    private String name;
+
+    @NotEmpty (message = "The category is required.")
+    private String category;
+
+
+    @Min(0)
+    private String price;
+
+    @NotEmpty (message = "The description is required.")
+    @Size(min = 10, message = "The description should be at least 10 characters")
+    @Size(max = 2000, message = "The description cannot exceed 2000 characters")
+    private String description;
+
+    public @NotEmpty(message = "The name is required.") String getName() {
+        return name;
+    }
+
+    public void setName(@NotEmpty(message = "The name is required.") String name) {
+        this.name = name;
+    }
+
+    public @NotEmpty(message = "The category is required.") String getCategory() {
+        return category;
+    }
+
+    public void setCategory(@NotEmpty(message = "The category is required.") String category) {
+        this.category = category;
+    }
+
+    @Min(0)
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public @Size(min = 10, message = "The description should be at least 10 characters") @Size(max = 2000, message = "The description cannot exceed 2000 characters") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Size(min = 10, message = "The description should be at least 10 characters") @Size(max = 2000, message = "The description cannot exceed 2000 characters") String description) {
+        this.description = description;
+    }
+}
