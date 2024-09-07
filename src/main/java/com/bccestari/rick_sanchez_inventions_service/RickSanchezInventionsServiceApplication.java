8fa518d6ba36e91f14c8b3a761f1b8601d87a1e5
Bruno Cestari <bccestari.project@gmail.com>
2024-09-06 22:14:08 -0300

    Initialize project structure and dependencies

---
+++ src/main/java/com/bccestari/rick_sanchez_inventions_service/RickSanchezInventionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bccestari.rick_sanchez_inventions_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickSanchezInventionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickSanchezInventionsServiceApplication.class, args);
+	}
+
+}
